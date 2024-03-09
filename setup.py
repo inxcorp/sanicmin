a7c75c80a3de495c3def785ee82105b5ac8ac2d2
Its-MatriX <acc.main.gamekiller@gmail.com>
2024-03-09 03:17:25 +0300

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sanicmin",
-    version="1.0.0",
+    version="1.0.2",
     packages=find_packages(),
     author="Infernox-Dev",
     description="A plugin for Sanic framework to minimize data transfer",
